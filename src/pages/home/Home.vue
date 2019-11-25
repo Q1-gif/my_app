6ee188e887aaf5554bc100e0ea226e361aa300ed
Q1-gif <3343172857@qq.com>
2019-11-25 11:23:54 +0800

    11-25-周一

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,20 +2,26 @@
     <div>
         <home-header></home-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
         <home-hot></home-hot>
+        <home-Liked></home-Liked>
     </div>
 </template>
 
 <script>
 import HomeHeader from './componets/header'
 import HomeSwiper from './componets/Swiper'
+import HomeIcons from './componets/Icons'
 import HomeHot from './componets/Hot'
+import HomeLiked from './componets/Liked'
 export default {
     name:'Home',
     components:{
         HomeHeader,
         HomeSwiper,
-        HomeHot
+        HomeIcons,
+        HomeHot,
+        HomeLiked
     }
 }
 </script>
