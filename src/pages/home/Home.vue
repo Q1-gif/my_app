7300ab80052329462e7ab63c14f4e2133bd80e1b
Q1-gif <3343172857@qq.com>
2019-11-25 10:38:13 +0800

    hot done

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,20 +2,20 @@
     <div>
         <home-header></home-header>
         <home-swiper></home-swiper>
-        <home-icons></home-icons>
+        <home-hot></home-hot>
     </div>
 </template>
 
 <script>
 import HomeHeader from './componets/header'
 import HomeSwiper from './componets/Swiper'
-import HomeIcons from './componets/Icons'
+import HomeHot from './componets/Hot'
 export default {
     name:'Home',
     components:{
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeHot
     }
 }
 </script>
